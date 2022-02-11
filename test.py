3a2919a5e099e12d855d580095b79c6ca1246f64
ec04012 <ec04012@uga.edu>
2022-02-11 13:00:28 -0500

    Implemented simple line drawing alg for horizontal lines

--- test.py
+++ test.py
@@ -2,14 +2,34 @@
 import pygame
 import pygame.gfxdraw
 from pygame.locals import (KEYDOWN, K_ESCAPE)
+from math import trunc
 import sys
 
 # Set up screen
 pygame.init()
 pygame.display.set_caption('CSCI 4810 Assignment 1')
-screen = pygame.display.set_mode((600, 600))
+screen = pygame.display.set_mode((1000, 1000))
 # Fill the background with white
 screen.fill((255, 255, 255))
+
+# x0 and y0 are the coordinates of the first point
+# x1 and y1 are the coordinates of the second point
+def simple_alg(x0, y0, x1, y1):
+    dx = x1-x0 # delta x
+    dy = y1-y0 # delta y
+    m = float(dy/dx) # find slope
+    for i in range(0,dx):
+        # calculate x and y of the point that we are trying to plot
+        x = x0 + i
+        y = m * i + y0
+        y = trunc(y)
+        #print("i: " + str(i))
+        #print("x: " + str(x))
+        #print("y: " + str(y))
+        #print("")
+        # draw the pixel in buffer (the line won't actullay show until pygame.display.flip() is called)
+        pygame.gfxdraw.pixel(screen, x, y, (255,0,0))
+    #print("================================================================")
 
 running = True
 while running:
@@ -23,10 +43,16 @@ while running:
             running = False
             pygame.quit()
             sys.exit()
-        
-    # Draw a sample line
-    for i in range(100,200):
-        pygame.gfxdraw.pixel(screen, i, i, (255,0,0))
+    
+    # working for horizontal lines (lines where dx > dy)
+    simple_alg(0,0, 800, 400)
+    simple_alg(0,1000, 800, 600)
+    simple_alg(0,200, 800, 200)
+
+    # not working for vertical lines
+    simple_alg(0,0, 400, 800) # pixels missing
+    # perfectly vertical lines crashes because of zero division
+    #simple_alg(600,0, 600, 1000)
 
     # update display
     pygame.display.flip()
