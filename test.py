ed52dd3989e0d675d3f0303f5e644ee74033dc7f
ec04012 <ec04012@uga.edu>
2022-02-10 20:22:31 -0500

    Refactored to use pygame

--- test.py
+++ test.py
@@ -1,22 +1,18 @@
 # importing Image class from PIL package
-from PIL import Image
-import time
+import pygame
+import pygame.gfxdraw
 
-# creating a object
-#im = Image.open(r"C:\Users\Eric\Desktop\Graphics\home.png")
-#im = Image.open(r"/mnt/c/Users/Eric/Desktop/Graphics/home.png")
-
-img = Image.new('RGB', (500, 500))
-pixels = img.load()
-for i in range(100,200):
-    pixels[i,i] = (255,0,0)
- 
-img.show()
-
-time.sleep(2)
-
-for i in range(100,200):
-    pixels[i+50,i] = (0,0,255)
- 
-img.show()
-img.save("test2.png")
+# Set up screen
+pygame.init()
+pygame.display.set_caption('CSCI 4810 Assignment 1')
+screen = pygame.display.set_mode((600, 600))
+done = False
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+    # Draw a sample line
+    for i in range(100,200):
+        pygame.gfxdraw.pixel(screen, i, i, (255,0,0))
+    # update display
+    pygame.display.flip()
