b7c74e1b81821442a3002528ace6f54eea1d3e5a
ec04012 <ec04012@uga.edu>
2022-02-11 14:07:41 -0500

    Implemented simple alg for vertical lines

--- test.py
+++ test.py
@@ -12,9 +12,22 @@ screen = pygame.display.set_mode((1000, 1000))
 # Fill the background with white
 screen.fill((255, 255, 255))
 
+red = (255,0,0)
+orange = (255,127,0)
+yellow = (255,255,0)
+green = (0,255,0)
+blue = (20,20,255)
+indigo = (75, 0, 130)
+violet = (178,30,241)
+
+white = (255,255,255)
+black = (10,10,10)
+cyan = (0,255,255)
+magenta = (255,255,0)
+
 # x0 and y0 are the coordinates of the first point
 # x1 and y1 are the coordinates of the second point
-def simple_alg(x0, y0, x1, y1):
+def simple_alg_hori(x0, y0, x1, y1, color):
     dx = x1-x0 # delta x
     dy = y1-y0 # delta y
     m = float(dy/dx) # find slope
@@ -28,8 +41,58 @@ def simple_alg(x0, y0, x1, y1):
         #print("y: " + str(y))
         #print("")
         # draw the pixel in buffer (the line won't actullay show until pygame.display.flip() is called)
-        pygame.gfxdraw.pixel(screen, x, y, (255,0,0))
+        pygame.gfxdraw.pixel(screen, x, y, color)
     #print("================================================================")
+
+# simple line drawing algorithm for vertical lines
+# x0 and y0 are the coordinates of the first point
+# x1 and y1 are the coordinates of the second point
+def simple_alg_vert(x0, y0, x1, y1, color):
+    dx = x1-x0 # delta x
+    dy = y1-y0 # delta y
+    m = float(dx/dy) # find slope
+    for i in range(0,dy):
+        # calculate x and y of the point that we are trying to plot
+        y = y0 + i
+        x = m * i + x0
+        x = trunc(x)
+        #print("i: " + str(i))
+        #print("x: " + str(x))
+        #print("y: " + str(y))
+        #print("")
+        # draw the pixel in buffer (the line won't actullay show until pygame.display.flip() is called)
+        pygame.gfxdraw.pixel(screen, x, y, color)
+    #print("================================================================")
+
+# x0 and y0 are the coordinates of the first point
+# x1 and y1 are the coordinates of the second point
+def simple_alg(x0, y0, x1, y1, color=red):
+    dx = abs(x1-x0) # |delta x|
+    dy = abs(y1-y0) # |delta y|
+    if dx>dy: #if the line is more horizontal than vertical
+        if x0>x1: # if x1 > x0, swap which point is p1 and which is p2
+            x0, x1 = x1, x0
+            y0, y1 = y1, y0
+        simple_alg_hori(x0, y0, x1, y1, color)
+    elif dy>dx: # If the line is more vertical than horizontal
+        if y0>y1: # if y1 > y0, swap which point is p1 and which is p2
+            x0, x1 = x1, x0
+            y0, y1 = y1, y0
+        simple_alg_vert(x0, y0, x1, y1, color)
+    
+# working for horizontal lines (lines where dx > dy)
+simple_alg(0,0, 900, 400, color=red)
+simple_alg(0,1000, 900, 600, color=orange)
+# Perfectly horizontal lines
+simple_alg(100,200, 900, 200, color=yellow)
+simple_alg(100,700, 800, 700, color=black)
+
+# not working for vertical lines
+simple_alg(0,0, 400, 800, color=green)
+simple_alg(200,1000, 600, 0, color=indigo)
+# perfectly vertical lines
+simple_alg(600,0, 600, 1000, color=blue)
+simple_alg(250,1000, 250, 0, color=violet)
 
 running = True
 while running:
@@ -43,16 +106,6 @@ while running:
             running = False
             pygame.quit()
             sys.exit()
-    
-    # working for horizontal lines (lines where dx > dy)
-    simple_alg(0,0, 800, 400)
-    simple_alg(0,1000, 800, 600)
-    simple_alg(0,200, 800, 200)
-
-    # not working for vertical lines
-    simple_alg(0,0, 400, 800) # pixels missing
-    # perfectly vertical lines crashes because of zero division
-    #simple_alg(600,0, 600, 1000)
 
     # update display
     pygame.display.flip()
